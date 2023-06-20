287c52da7ac29dc9f3809753a7092b3749ab4459
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 17:45:18 -0300

    Criação de funcionalidade de criar carta

---
+++ script.js
@@ -0,0 +1,18 @@
+const input = document.getElementById('carta-texto');
+const btnCriar = document.getElementById('criar-carta');
+const cartaGerada = document.getElementById('carta-gerada');
+
+function criarCarta() {
+    cartaGerada.innerText = '';
+    const text = input.value;
+    const listText = text.split(' ');
+    for (let i = 0; i < listText.length; i += 1) {
+        const elementText = listText[i];
+        const element = document.createElement('span');
+        element.innerText = elementText;
+        cartaGerada.appendChild(element);
+        input.value = '';
+    }
+}
+
+btnCriar.addEventListener('click', criarCarta);
