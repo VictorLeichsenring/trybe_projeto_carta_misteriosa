2e478ab5c58d4c22bb937d39cd7d03131b6cf153
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 17:54:24 -0300

    Criação de mensagem em caso do input ser vazio ou preenchio com espaços em branco

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const btnCriar = document.getElementById('criar-carta');
 const cartaGerada = document.getElementById('carta-gerada');
 
 function criarCarta() {
-    cartaGerada.innerText = '';
-    const text = input.value;
+  cartaGerada.innerText = '';
+  const text = input.value;
+  if (text.trim() === '') {
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
     const listText = text.split(' ');
     for (let i = 0; i < listText.length; i += 1) {
-        const elementText = listText[i];
-        const element = document.createElement('span');
-        element.innerText = elementText;
-        cartaGerada.appendChild(element);
+      const elementText = listText[i];
+      const element = document.createElement('span');
+      element.innerText = elementText;
+      cartaGerada.appendChild(element);
     }
+  }
 }
 
 btnCriar.addEventListener('click', criarCarta);
