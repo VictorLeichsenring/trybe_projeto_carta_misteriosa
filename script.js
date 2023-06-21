8ea7b9b0d6217a53c31b987f92c902bfc07028b2
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 22:21:20 -0300

    Criada função para gerar nova combinação de classes

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
 
+
 function generateRandonInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function changeElement(event) {
+  const element= event.target;
+  while (element.classList.length > 0) {
+    element.classList.remove(element.classList.item(0));
+  }
+  const classStyle = grupoEstilo[generateRandonInt(0, grupoEstilo.length)];
+  const classSize = grupoTamanho[generateRandonInt(0, grupoTamanho.length)];
+  const classRotation = grupoRotacao[generateRandonInt(0, grupoRotacao.length)];
+  const classInclination = grupoInclinacao[generateRandonInt(0, grupoInclinacao.length)];
+  element.classList.add(classStyle);
+  element.classList.add(classSize);
+  element.classList.add(classRotation);
+  element.classList.add(classInclination);
 }
 
 function criarCarta() {
@@ -25,12 +41,12 @@ function criarCarta() {
       const classSize = grupoTamanho[generateRandonInt(0, grupoTamanho.length)];
       const classRotation = grupoRotacao[generateRandonInt(0, grupoRotacao.length)];
       const classInclination = grupoInclinacao[generateRandonInt(0, grupoInclinacao.length)];
-      element.classList.add(classStyle)
-      element.classList.add(classSize)
-      element.classList.add(classRotation)
-      element.classList.add(classInclination)
-      
+      element.classList.add(classStyle);
+      element.classList.add(classSize);
+      element.classList.add(classRotation);
+      element.classList.add(classInclination);
       cartaGerada.appendChild(element);
+      element.addEventListener('click', changeElement);
     }
   }
 }
