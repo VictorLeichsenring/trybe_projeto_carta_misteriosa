2f2ced42bad20e6b382aa39d85113e72d1431151
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 22:45:30 -0300

    Criação de contador de palavras

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const input = document.getElementById('carta-texto');
 const btnCriar = document.getElementById('criar-carta');
 const cartaGerada = document.getElementById('carta-gerada');
+const contador = document.getElementById('carta-contador');
 const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
 const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
-
 
 function generateRandonInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function changeElement(event) {
-  const element= event.target;
+  const element = event.target;
   while (element.classList.length > 0) {
     element.classList.remove(element.classList.item(0));
   }
@@ -28,26 +28,22 @@ function changeElement(event) {
 
 function criarCarta() {
   cartaGerada.innerText = '';
-  const text = input.value;
-  if (text.trim() === '') {
+  if (input.value.trim() === '') {
     cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    const listText = text.split(' ');
+    const listText = input.value.split(' ');
     for (let i = 0; i < listText.length; i += 1) {
-      const elementText = listText[i];
       const element = document.createElement('span');
-      element.innerText = elementText;
+      element.innerText = listText[i];
       const classStyle = grupoEstilo[generateRandonInt(0, grupoEstilo.length)];
       const classSize = grupoTamanho[generateRandonInt(0, grupoTamanho.length)];
       const classRotation = grupoRotacao[generateRandonInt(0, grupoRotacao.length)];
       const classInclination = grupoInclinacao[generateRandonInt(0, grupoInclinacao.length)];
-      element.classList.add(classStyle);
-      element.classList.add(classSize);
-      element.classList.add(classRotation);
-      element.classList.add(classInclination);
+      element.classList.add(classStyle, classSize, classRotation, classInclination);
       cartaGerada.appendChild(element);
       element.addEventListener('click', changeElement);
     }
+    contador.innerText = cartaGerada.children.length;
   }
 }
 
